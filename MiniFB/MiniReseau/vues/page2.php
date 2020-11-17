36db00530b73fa1d20dfd9623894b40cc34a920f
HervelyneV <53907849+HervelyneV@users.noreply.github.com>
2020-11-17 10:32:20 +0100

    import -file

---
+++ MiniFB/MiniReseau/vues/page2.php
@@ -0,0 +1,12 @@
+<header>
+
+    
+<img src="logo.png" alt="LOGO DU SITE"  >
+<img src="home.png" alt="ICONE HOME">
+<img src="profil.png" alt="ICONE PROFIL">
+<img src="parametre.png" alt="ICONE PARAMETRE">
+
+<input type="search" placeholder="Chercher un Amoose" name="rechercher">
+
+
+</header>
