699d339293d32a37e8a635db90bdf9029a9f63b5
Tiran-Zelia <tiran.zelia@gmail.com>
2020-11-19 09:40:09 +0100

    Creation de la page mur et integration2

--- MiniFB/MiniReseau/config/actions.php
+++ MiniFB/MiniReseau/config/actions.php
@@ -7,5 +7,5 @@ $listeDesActions = array(
     "login" => "vues/login.php",
     "connexion" => "traitement/connexion.php",
     "deconnexion" => "traitement/deconnexion.php",
-    
+    "mur" => "vues/mur.php",
 );
---
+++ MiniFB/MiniReseau/vues/mur.php
@@ -0,0 +1,38 @@
+<link rel="stylesheet" href="../css/mur_style.css">
+
+
+<header>
+<div id="header">
+    
+<img src="logo.png" alt="LOGO DU SITE"  >
+<img src="home.png" alt="ICONE HOME">
+<img src="profil.png" alt="ICONE PROFIL">
+<img src="parametre.png" alt="ICONE PARAMETRE">
+
+<input type="search" placeholder="Chercher un Amoose" name="rechercher">
+</div>
+
+</header>
+
+<main>
+
+<div id="mur">
+    <div id="statut">
+    <h1>THEO CREMERS</h1>
+    
+    <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard Le Lorem Ipsum est simplement Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum </p>
+    </div>
+    
+    
+    
+    
+</div>
+
+
+
+
+
+
+
+
+</main>
