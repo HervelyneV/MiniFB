6135cc745458a7636a5951d1cb2a1328f3d0d3ad
vasse <hervelyne.v@gmail.com>
2020-11-19 08:35:06 +0100

    ajout page login

---
+++ MiniReseau/vues/signin.php
@@ -0,0 +1,24 @@
+
+<link rel="stylesheet" href="../css/signin_style.css">
+
+<section id="signin">
+    <div id="signin_champ">
+        <h2>Inscrit toi</h2>
+        <form action="index.php?action=inscription" method="post">
+            <input type="text" placeholder="Adresse Mail" name="mail" required/> <br>
+            <input type="text" placeholder="Identifiant" name="login" required/> <br>
+            <input type="password" placeholder="Mot de passe" name="password" required/> 
+            <br>
+            <input id="envoyer_input" type="submit" name="Connexion" value="Inscription"/>
+        </form>
+    </div>
+    
+    <div id="right_moose">
+        <h1>Moose</h1>
+        <div>
+            <p>Bienvenue l'ami !</p>
+            <img src="css/src/moose.png">
+        </div>
+    </div>
+    
+</section>
