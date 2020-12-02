479db9ffad6b3ad6f6a9154423451fb8e40a3d63
Cremers Théo <theo.cremers59@gmail.com>
2020-12-02 15:18:25 +0100

    test

--- MiniReseau/vues/404.php
+++ MiniReseau/vues/404.php
@@ -1,1 +1,12 @@
-Désolé, cette page est introuvable !
+<section>
+    <div class="vert">
+        <h1>OUPS...</h1>
+        <h2>ERREUR 404</h2>
+        <a>Retour</a>
+        <div>
+            <p>On dirait qu'il y a un problème...</p>
+            <img src="css/src/moose.png">
+        </div>
+    </div>
+
+</section>
