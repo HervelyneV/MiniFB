49bf0086bd05a25cc7f0c9b9967359da0e273d66
vasse <hervelyne.v@gmail.com>
2020-12-03 08:18:58 +0100

    finition inscription

--- MiniReseau/index.php
+++ MiniReseau/index.php
@@ -14,7 +14,7 @@ ob_start(); // Je démarre le buffer de sortie : les données à afficher sont s
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ma super application</title>
+    <title>Moose</title>
 
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/login_style.css">
