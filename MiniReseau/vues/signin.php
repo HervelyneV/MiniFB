2e72f15cf5e17da6f89b724b3410bb9d87f6cafd
vasse <hervelyne.v@gmail.com>
2020-12-03 09:43:30 +0100

    css page accueil

--- MiniReseau/vues/signin.php
+++ MiniReseau/vues/signin.php
@@ -5,39 +5,12 @@
     <div id="signin_champ">
         <h2>Inscrit toi</h2>
         <form  name="inscription" action="index.php?action=inscription" method="post">
-            <?php
-            //si erreur sur le mail on affiche l'erreur
             
-            if (isset($mainotok)){
-            ?>
-            <div> <?= $mailnotok ?> </div>
-            <?php
-            }
-            ?>
-            <input type="text" placeholder="Adresse Mail" name="mail" value="<?php if(isset($mail)) { echo $mail; }?>" required/> <br>
+            <input type="text" placeholder="Adresse Mail" name="mail" value="" required/> <br>
             
-            <?php
-            //si erreur sur le login on affiche l'erreur
-            
-            if (isset($loginnotok)){
-            ?>
-            <div> <?= $loginnotok ?> </div>
-            <?php
-            }
-            ?>
-            <input type="text" placeholder="Identifiant" name="login" value="<?php if(isset($login)) { echo $login; }?>" required/> <br>
-            
-            <?php
-            //si erreur sur le mdp on affiche l'erreur
-            
-            if (isset($passwordnotok)){
-            ?>
-            <div> <?= $passwordnotok ?> </div>
-            <?php
-            }
-            ?>
-            
-            <input type="password" placeholder="Mot de passe" name="password" value="<?php if(isset($password)) { echo $password; }?>" required/> 
+            <input type="text" placeholder="Identifiant" name="login" value="" required/> <br>
+       
+            <input type="password" placeholder="Mot de passe" name="password" value="" required/> 
             <br>
             <input id="envoyer_input" type="submit" name="Connexion" value="Inscription"/>
         </form>
@@ -47,10 +20,10 @@
         <h1>Moose</h1>
         <div>
             <p>Bienvenue l'ami !</p>
-            <img src="css/src/moose.png">
+            <img src="../css/src/moose.png">
         </div>
     </div>
     
 </section>
 
-/* 
+ 
