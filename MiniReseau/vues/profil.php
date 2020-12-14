7e955b72a41dfcfeb61befb35a01eb6cabf97ae0
vasse <hervelyne.v@gmail.com>
2020-12-14 17:19:03 +0100

    continuité profil

--- MiniReseau/vues/profil.php
+++ MiniReseau/vues/profil.php
@@ -13,28 +13,6 @@
             <h2 id="login_title"><?php echo $_SESSION["login"]; ?></h2>
               <a href="index.php?action=deconnexion" id="lien_deco">Déconnexion</a>
         </div>
-         <div id="liste_amis">
-            <ul>
-                <?php
-                     $sql ="SELECT * FROM user WHERE id IN ( SELECT user.id FROM user INNER JOIN lien ON idUtilisateur1=user.id AND etat='amis' AND idUtilisateur2=? UNION SELECT user.id FROM user INNER JOIN lien ON idUtilisateur2=user.id AND etat='amis' AND idUtilisateur1=?)";
-
-                     $q = $pdo->prepare($sql);
-
-                     $q->execute(array($_SESSION["id"], $_SESSION["id"]));
-
-                     while($line = $q->fetch()){
-    
-                 ?>
-                
-                <li><a href="index.php?action=profil&id=<?php echo $line["id"]; ?>" class="lien_ami"><?php echo $line["login"];?></a></li>
-             
-                 <?php
-                     }
-                 ?>
-            </ul>
-        </div>
-
-      
     </div>
 
     <div id="profil_content">
@@ -59,6 +37,29 @@
                         <span id="profil_login"><?php  echo ucwords($line["login"]); ?></span>
                     </div>
                 </div>
+            
+             <div id="liste_amis">
+            <ul>
+                 <h3 id="titre_amis">Amis</h3>
+                <?php
+                     $sql ="SELECT * FROM user WHERE id IN ( SELECT user.id FROM user INNER JOIN lien ON idUtilisateur1=user.id AND etat='amis' AND idUtilisateur2=? UNION SELECT user.id FROM user INNER JOIN lien ON idUtilisateur2=user.id AND etat='amis' AND idUtilisateur1=?)";
+
+                     $q = $pdo->prepare($sql);
+
+                     $q->execute(array($_SESSION["id"], $_SESSION["id"]));
+
+                     while($line = $q->fetch()){
+    
+                 ?>
+               
+                <li>
+                    <a href="index.php?action=profil&id=<?php echo $line["id"]; ?>" class="lien_ami"><?php echo $line["login"];?></a></li>
+             
+                 <?php
+                     }
+                 ?>
+            </ul>
+        </div>
                 
                 <?php
                     
@@ -67,7 +68,102 @@
                     
                     if($_GET["id"]==$_SESSION["id"]){
                         $ok = true;
-                    }else{
+            
+                        $sql_demande = "SELECT user.* FROM user WHERE id IN(SELECT idUtilisateur1 FROM lien WHERE idUtilisateur2=? AND etat='attente') ORDER BY user.login";
+
+                        $q3 = $pdo->prepare($sql_demande);
+
+                        $q3->execute(array($_SESSION["id"]));
+
+                        while($line3 = $q3->fetch()){
+                            /*echo "<pre>";
+                            print_r($line3);
+                            echo "</pre>";*/
+                            ?>
+                            <div class="voir_ami">
+                                <span class="ami_name"><?php echo $line3["login"]; ?></span>
+                                <span class="etat_ami">Demande reçue</span>
+                                <a class="bouton-accept" href="index.php?action=accept&id=<?php echo $line3["id"]; ?>">Accepter</a>
+                                <a class="bouton-reject" href="index.php?action=reject&id=<?php echo $line3["id"]; ?>">Refuser</a>
+                            </div>
+                            <?php
+                        }
+                    ?>
+                         <div id="ecrit_post">
+                    <form action="index.php?action=ajouterpost" method="post">
+                        <textarea id="content" name="content" placeholder="Racontez-nous vos histoire les plus dingues..." required></textarea>
+                        <?php
+                            if($line["id"] != $_SESSION["id"]){
+                                echo "<input type='hidden' name='idAmi' value='".$line['id']."' />";
+                            }
+                        ?>
+                        <input type="submit" value="Envoyer">
+                    </form>
+                </div>
+            
+            <div id="profil_post">
+                    <h3 id="title_profil_post">Mes posts</h3>
+                    <?php
+                        //Liste des posts
+                        $sql_posts = "SELECT ecrit.*, user.*, ecrit.id AS ID FROM ecrit JOIN user ON user.id=ecrit.idAmi WHERE ecrit.idAuteur=? OR ecrit.idAmi=? ORDER BY ecrit.dateEcrit DESC";
+
+                        $q_posts = $pdo->prepare($sql_posts);
+                        
+                        $q_posts->execute(array($_GET["id"], $_GET["id"]));
+                        
+                        while($line_posts = $q_posts->fetch()){
+                            /*echo "<pre>";
+                            var_dump($line_posts);
+                            echo "</pre>";*/
+                    ?>
+                    <div class="post_perso">
+                        <div class="main_post">
+                            <div class="ecrit_post">
+                                <p class="login_auteur"><?php echo $line_posts["login"]; ?></p>
+                                <p class="title_auteur"><?php echo $line_posts["titre"]; ?></p>
+                                <p class="post_auteur"><?php echo $line_posts["contenu"]; ?></p>
+                                <p class="date_post">Posté par <?php echo $line_posts["login"]; ?> le <?php echo $line_posts["dateEcrit"]; ?></p>
+                            </div>
+                        </div>
+                        <div class="commentaire_post">
+                            <h4 class="conmmentaire_title">Commentaires</h4>
+                            
+                            <div id="ecrit-commentaire">
+                                <form action="index.php?action=ajoutCommentaire" method="post">
+                                    <textarea id="contenu" name="contenu" placeholder="Commentez ici..." required></textarea>
+                                    <input type="hidden" name="idPost" value="<?php echo $line_commentaire["idPost"]; ?>" />
+                                    <input type="submit" value="Envoyer">
+                                </form>
+                            </div>
+                            
+                            <?php
+                                //Liste des posts
+                                $sql_comments = "SELECT commentaire.*, user.* FROM commentaire JOIN user ON user.id=commentaire.idUser WHERE commentaire.idPost=? ORDER BY commentaire.dateCommentaire DESC";
+
+                                $q_comments = $pdo->prepare($sql_comments);
+
+                                $q_comments->execute(array($line_posts["idPost"]));
+
+                                while($line_comments = $q_comments->fetch()){
+                                    /*echo "<pre>";
+                                    var_dump($line_comments);
+                                    echo "</pre>";*/
+                            ?>
+                            <div class="commentaire">
+                                <div class="main_commentaire">
+                                    <p class="commentaire_user"><?php echo $line_commentaire["contenu"]; ?></p>
+                                    <p class="date_commentaire">Posté par <?php echo $line_commentaire["login"]; ?> le <?php echo $line_comments["dateCommentaire"]; ?></p>
+                                </div>
+                            </div>
+                            <?php
+                                }
+                  
+                        }
+
+              }
+  
+                    
+                    else{
                         // Verifions si on est amis avec cette personne
                         $sql_verif = "SELECT * FROM lien WHERE (idUtilisateur1=? AND idUtilisateur2=?) OR (idUtilisateur1=? AND idUtilisateur2=?)";
                         $q_verif = $pdo->prepare($sql_verif);
@@ -178,7 +274,7 @@
                     if($line["id"] == $_SESSION["id"]){
                         $q4->execute(array($_SESSION["id"], $_SESSION["id"]));
                     }else{
-                        $q4->execute(array($_GET["id_profil"], $_GET["id_profil"]));
+                        $q4->execute(array($_GET["id"], $_GET["id"]));
                     }
                         
 
